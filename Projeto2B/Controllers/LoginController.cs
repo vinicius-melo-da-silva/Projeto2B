67c27506a0fba132eec2a98444d4f47f0374daf7
Vinicius <viniciusmsilva.ds@gmail.com>
2025-03-19 20:32:31 -0300

    Criando a estrutura de classes do projeto

---
+++ Projeto2B/Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto2B.Controllers
+{
+    //O nome da controller vai ser sempre PascalCase: MinhaVariavel, camelCase: minhaVariavel
+
+    public class LoginController : Controller //Controller sempre vai ser publica
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
