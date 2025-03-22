9c29332e7de502fb7ce406297b800a7643e5b138
Vinicius <viniciusmsilva.ds@gmail.com>
2025-03-21 22:13:59 -0300

    Criando metodo obter e adicionar usuário

--- Projeto2B/Repositorio/UsuarioRepositorio.cs
+++ Projeto2B/Repositorio/UsuarioRepositorio.cs
@@ -1,6 +1,63 @@
-﻿namespace Projeto2B.Repositorio
+﻿using Projeto2B.Models;
+
+//Repositório sempre vai usar uma model
+
+namespace Projeto2B.Repositorio
 {
     public class UsuarioRepositorio
     {
+        //Preparando a estrutura da conexão com o banco de dados
+
+        private readonly string _connectionString;
+
+        //Construtor
+        public UsuarioRepositorio(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection"); //Default é padrão
+        }
+
+        // Criando o metodo adicionar usuario
+        public void AdicionarUsuario(Usuario usuario)
+        {
+            using (var db = new Conexao(_connectionString))
+            {
+                var cmd = db.MySqlCommand();
+                cmd.CommandText = "INSERT INTO Usuario (Nome, Email, Senha) VALUES (@Nome,@Email,@Senha)";
+                cmd.Parameters.AddWithValue("@Nome", usuario.Nome);
+                cmd.Parameters.AddWithValue("@Email", usuario.Email);
+                cmd.Parameters.AddWithValue("@Senha", usuario.Senha);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        // Criando metodo de obter usuário
+        public Usuario ObterUsuario(string email)
+        {
+            using (var db = new Conexao(_connectionString))
+            {
+                var cmd = db.MySqlCommand();
+                cmd.CommandText = "SELECT * FROM Usuario WHERE Email = @Email";
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.ExecuteNonQuery();
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return new Usuario
+                        {
+                            Id = reader.GetInt32("Id"),
+                            Nome = reader.GetString("Nome"),
+                            Email = reader.GetString("Email"),
+                            Senha = reader.GetString("Senha"),
+
+                        };
+
+                    }
+
+                }
+                return null;
+            }
+        }
     }
 }
