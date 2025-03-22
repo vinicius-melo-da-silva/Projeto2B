38161fec1ee12f81520253783e9a2d3d9108642b
Vinicius <viniciusmsilva.ds@gmail.com>
2025-03-21 21:47:02 -0300

    Criando a classe de conexão com o banco

--- Projeto2B/Repositorio/Conexao.cs
+++ Projeto2B/Repositorio/Conexao.cs
@@ -1,6 +1,33 @@
-﻿namespace Projeto2B.Repositorio
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Projeto2B.Repositorio
 {
-    public class Conexao
+    public class Conexao : IDisposable
     {
+        private MySqlConnection _connection;
+
+        //Passagem dos dados
+        public MySqlCommand MySqlCommand()
+        {
+            return _connection.CreateCommand();
+        }
+
+        //Abre a conexão
+        public Conexao(string connectionString)
+        {
+            _connection = new MySqlConnection(connectionString);
+            _connection.Open();
+        }
+
+        // Fecha a conexão
+        public void Dispose()
+        {
+            if (_connection != null && _connection.State == ConnectionState.Open)
+            {
+                _connection.Close();
+                _connection.Dispose();
+            }
+        }
     }
 }
